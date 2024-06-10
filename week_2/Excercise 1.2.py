bad2a501023af61b6ccb71817f1f5d9440bcbdde
Nadia <nadiagainer@gmail.com>
2024-06-09 21:34:53 -0500

    update

---
+++ week_2/Excercise 1.2.py
@@ -0,0 +1,8 @@
+"""
+Author: Nadia Gainer
+Date: 6/9/24
+File Name: Excercise 1.2.py
+Description: Building Your First Python Program
+"""
+
+print("Let your fear of regret be stronger than your fear of failure. ― Ruth Soukup")
