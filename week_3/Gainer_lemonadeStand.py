48c335572e9d12954b14dc4b8927a10ada512a67
Nadia <nadiagainer@gmail.com>
2024-06-16 06:40:24 -0500

    update 2

---
+++ week_3/Gainer_lemonadeStand.py
@@ -0,0 +1,17 @@
+"""
+Author: Nadia Gainer
+Date: 6/16/24
+File Name: Gainer_LemonadeStand.py
+Description: lemonade stand file
+"""
+
+def calculate_cost(lemons_cost,sugar_cost)
+ total_cost = lemons_cost + sugar_cost # Calculating the total cost
+ return total_cost # Returning the total cost
+
+
+def calculate_profit (lemons_cost,sugar_cost,selling_price)
+ profit = 
+ return profit 
+
+print()
