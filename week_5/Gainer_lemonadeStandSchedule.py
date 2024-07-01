9437d42d90748fd06479733d9ee14610ffbe8f30
Nadia <nadiagainer@gmail.com>
2024-06-30 20:10:08 -0500

    update

--- week_5/Gainer_lemonadeStandSchedule.py
+++ week_5/Gainer_lemonadeStandSchedule.py
@@ -5,15 +5,15 @@ File Name: gainer_lemonadestandschedule.py
 Description: lemonadestand schedule
 """
 
-tasks = ["make lemonade", "promote lemonade stand", "clean lemonade stand", "open lemonade stand", "close lemonade stand"]
+tasks = ["make lemonade", "promote lemonade stand", "clean lemonade stand", "open lemonade stand", "close lemonade stand"]//tasks//
 def lemonadetasks():
     for task in tasks:
         print(task)
 
 lemonadetasks()
-days =["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+days =["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]//days of the week// 
 for i,day in enumerate(days):
     if day in ["saturday","sunday"]:
-        print("off day")
+        print("off day") //states days off for saturday and sunday// 
     else:
-        print(f"{day}-{tasks[i]}")
+        print(f"{day}-{tasks[i]}") //print that task for each day of the week// 
